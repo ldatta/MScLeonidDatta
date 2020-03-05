46c580bf7898928a7cf098e5f0bd7ae85be35ae0
ldatta <45818853+ldatta@users.noreply.github.com>
2020-03-05 22:39:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -81,7 +81,8 @@ def main():
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--cuda', type=bool, default=True)
+    parser.add_argument('--no-cuda', action='store_true', default=True,
+                        help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
