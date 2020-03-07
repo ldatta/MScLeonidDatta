f0f1d86c46202a3690d228a23bacfc0b284ee870
ldatta <45818853+ldatta@users.noreply.github.com>
2020-03-07 21:08:44 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -235,7 +235,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=10000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=20, metavar='N',
+    parser.add_argument('--epochs', type=int, default=5, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
@@ -441,11 +441,11 @@ def main():
     #print("CONVOLUTION NET")
     #model = Netconv().to(device)
     
-    #print("CONVOLUTION NET for 28x28")
-    #model = Net28().to(device)
+    print("CONVOLUTION NET for 28x28")
+    model = Net28().to(device)
     
-    print("ATTENTION NET")
-    model = NetAtt().to(device)
+    #print("ATTENTION NET")
+    #model = NetAtt().to(device)
 
 
 
