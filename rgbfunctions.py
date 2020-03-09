cf45426de4d08cf0a8242132701bb25ec00caa30
ldatta <45818853+ldatta@users.noreply.github.com>
2020-03-09 20:56:45 +0100

    Create rgbfunctions.py

---
+++ rgbfunctions.py
@@ -0,0 +1,121 @@
+def plotgraph (xs,y1s,y2s,yts):
+    plt.clf()
+    
+    fig = plt.figure(figsize=(10, 7))
+    plt.plot(xs,y1s,'s:r')
+    plt.ylim(0, 100)
+    #plt.xlim(0,20)
+    fig.suptitle('ACCURACY GRAPH')
+    plt.xlabel('Epoch no')
+    plt.ylabel('Accuracy %')
+    for x,y in zip(xs,y1s):
+        label = "{:.0f}".format(y)
+        plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+    
+    plt.plot(xs,y2s,'^:g')
+    for x,y in zip(xs,y2s):
+        label = "{:.0f}".format(y)
+        plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+    plt.plot(xs,yts,'*:b')
+    for x,y in zip(xs,yts):
+        label = "{:.0f}".format(y)
+        plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') 
+    blue_line = mlines.Line2D([], [], color='blue', marker='*',
+                          markersize=10, label='training data')
+    red_line = mlines.Line2D([], [], color='red', marker='s',
+                          markersize=10, label='Red test data')
+    green_line = mlines.Line2D([], [], color='green', marker='^',
+                          markersize=10, label='Green test data')
+    plt.legend(handles=[blue_line,red_line,green_line],loc=2)
+    plt.show()   
+    
+    
+def train(args, model, device, train_loader, optimizer, epoch, hortest_loader,test_loader):
+    r=0
+    g=0
+    t=0
+    running_loss = 0
+    total_train = 0
+    correct_train = 0
+    model.train() 
+    
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        #print(output.shape)
+        
+        #print(target.shape)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+# =============================================================================
+#         if (epoch==20):
+#             weight1 = model.conv1.weight.data.numpy()
+#             np.save('LtrainedWeight1.npy',weight1)
+#             weight2 = model.conv2.weight.data.numpy()
+#             np.save('LtrainedWeight2.npy',weight2)
+#             weight3 = model.conv3.weight.data.numpy()
+#             np.save('LtrainedWeight3.npy',weight3)
+#             weight4 = model.conv4.weight.data.numpy()
+#             np.save('LtrainedWeight4.npy',weight4)
+#             weight5 = model.conv5.weight.data.numpy()
+#             np.save('LtrainedWeight5.npy',weight5)
+# =============================================================================
+        running_loss += loss.item()
+        _, predicted = torch.max(output.data, 1)
+        total_train += target.size(0)
+        correct_train += predicted.eq(target.data).sum().item()
+        train_accuracy = 100 * correct_train / total_train
+        q=int(100. * batch_idx / len(train_loader))
+            
+        if batch_idx % args.log_interval == 0:
+            if batch_idx % args.log_interval == 0:
+                if (q%10) == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} \taccuracy: {:.2f}%'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(),train_accuracy))
+                if (q==50):
+                    r=test(args, model, device, hortest_loader)
+                    g=test(args, model, device, test_loader)
+                    t=int(train_accuracy)   
+    
+    return [r,g,t,train_accuracy]
+
+
+def test(args, model, device, test_loader):
+    #model.eval()
+    model.train(mode=False)
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            #np.save('predlabelLtrainedINLtest.npy',pred)
+            #print(pred.shape,"predicted test")
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    acc=int(100. * correct / len(test_loader.dataset))
+    return acc
+
