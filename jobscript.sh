509a67d977691ef15ef49945a77fb2016ab78b62
ldatta <45818853+ldatta@users.noreply.github.com>
2020-03-09 02:04:42 +0100

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -37,5 +37,5 @@
 #module load matlab/R2018b
 
 # Complex or heavy commands should be started with 'srun' (see 'man srun' for more information)
-python3 testmain.py $*
+python3 testmain2.py $*
 # Use this simple command to check that your sbatch settings are working (verify the resources allocated in the usage statistics)
