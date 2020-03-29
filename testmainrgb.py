4c252a6143218e194bf32e86446a5877c6116ab0
ldatta <45818853+ldatta@users.noreply.github.com>
2020-03-29 10:13:57 +0200

    Update testmainrgb.py

--- testmainrgb.py
+++ testmainrgb.py
@@ -33,8 +33,8 @@ def cor(img,img2):
 
 add=64
 
-k=4
-k2=4
+k=1
+k2=1
 class NetconvDepw2(nn.Module):
     def __init__(self):
         super(NetconvDepw2, self).__init__()
@@ -539,9 +539,9 @@ def main():
     resulttrn[2::2] = trnacc
     e=(np.arange(0,(args.epochs+0.5),0.5 ))
     #plotgraph(e,resultred,resultgrn, resulttrn)# ,bresultred,bresultgrn, bresulttrn)
-    np.save('R7trainRGBWorkingK24red.npy',resultred)
-    np.save('R7trainRGBWorkingK24grn.npy',resultgrn)
-    np.save('R7trainRGBWorkingK24trn.npy',resulttrn)
+    np.save('R7trainRGBWorkingK21red.npy',resultred)
+    np.save('R7trainRGBWorkingK21grn.npy',resultgrn)
+    np.save('R7trainRGBWorkingK21trn.npy',resulttrn)
     
     #bresultred=np.load('Baseresults/INLtrainedresultred.npy')
     #bresultgrn=np.load('Baseresults/INLtrainedresultgrn.npy')  
