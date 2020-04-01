92d9a2b1a3dde1f9a2610a5e4e84ff3614f23fe9
ldatta <45818853+ldatta@users.noreply.github.com>
2020-04-01 20:46:20 +0200

    Update testmainrgb.py

--- testmainrgb.py
+++ testmainrgb.py
@@ -15,9 +15,9 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-k=13
-k2=13
-k3=13
+k=14
+k2=14
+k3=14
 
 class NetconvDep(nn.Module):
     def __init__(self):
@@ -432,9 +432,9 @@ def main():
 #     np.save('notun22GLRGBKK2K313maskgap5grn.npy',resultgrn)
 #     np.save('notun22GLRGBKK2K313maskgap5trn.npy',resulttrn)
     
-    np.save('notun22R7RGBKK2K313maskgap5red.npy',resultred)
-    np.save('notun22R7RGBKK2K313maskgap5grn.npy',resultgrn)
-    np.save('notun22R7RGBKK2K313maskgap5trn.npy',resulttrn)
+    np.save('notun22R7RGBKK2K314maskgap5red.npy',resultred)
+    np.save('notun22R7RGBKK2K314maskgap5grn.npy',resultgrn)
+    np.save('notun22R7RGBKK2K314maskgap5trn.npy',resulttrn)
     
     #bresultred=np.load('Baseresults/INLtrainedresultred.npy')
     #bresultgrn=np.load('Baseresults/INLtrainedresultgrn.npy')  
