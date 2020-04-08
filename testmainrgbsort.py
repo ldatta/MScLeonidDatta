84b9a33393409dcb81d8008e0ed23719f4a14ce7
ldatta <45818853+ldatta@users.noreply.github.com>
2020-04-08 21:41:06 +0200

    Update testmainrgbsort.py

--- testmainrgbsort.py
+++ testmainrgbsort.py
@@ -22,21 +22,21 @@ import matplotlib.pyplot as plt
 
 import math 
 
-GL=0 #SET GL=0 for Red-7-shaped training Data , Set GL=1 for Green-L-shaped training Data
+GL=1 #SET GL=0 for Red-7-shaped training Data , Set GL=1 for Green-L-shaped training Data
 
 k=14 #k, k2 and k3 controls the number of channels 
 k2=14
 k3=14
 
 def npsave(resultred,resultgrn,resulttrn): #this function saves the result
-#     np.save('GLRGBnewWsortred.npy',resultred)
-#     np.save('GLRGBnewWsortgrn.npy',resultgrn)
-#     np.save('GLRGBnewWsorttrn.npy',resulttrn)
-#     print("hello GL=1")
-    np.save('R7RGBnewWsortred.npy',resultred)
-    np.save('R7RGBnewWsortgrn.npy',resultgrn)
-    np.save('R7RGBnewWsorttrn.npy',resulttrn)
-    print("hello GL=0")
+    np.save('GLRGBnewWsortred.npy',resultred)
+    np.save('GLRGBnewWsortgrn.npy',resultgrn)
+    np.save('GLRGBnewWsorttrn.npy',resulttrn)
+    print("hello GL=1")
+#     np.save('R7RGBnewWsortred.npy',resultred)
+#     np.save('R7RGBnewWsortgrn.npy',resultgrn)
+#     np.save('R7RGBnewWsorttrn.npy',resulttrn)
+#     print("hello GL=0")
 
 #     np.save('GLRGBsortred.npy',resultred)
 #     np.save('GLRGBsortgrn.npy',resultgrn)
