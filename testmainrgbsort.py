8718ea1d7d95d91a7ae525d4a251215f63eeea56
ldatta <45818853+ldatta@users.noreply.github.com>
2020-04-16 09:25:32 +0200

    Update testmainrgbsort.py

--- testmainrgbsort.py
+++ testmainrgbsort.py
@@ -123,7 +123,7 @@ def train(args, model, device, train_loader, optimizer, epoch, hortest_loader,te
     correct_train = 0
     model.train() 
       
-    model.conv1.weight.data=x1.to(device)
+#     model.conv1.weight.data=x1.to(device)
 #     model.conv11.weight.data=x11.to(device)
 #     model.conv2.weight.data=x2.to(device)
 #     model.conv22.weight.data=x22.to(device)
